f0dfd859c485df1d8361f709ac5d881f5301168b
Magnus Blåudd <blaudden@users.noreply.github.com>
2023-11-23 18:54:01 +0100

    Sajtens url
    Sajten heter mountainbikeorientering.se. Den finns där med en redirect
    till www.mountainbikeorientering.se där den lever, det sker automatiskt
    och ingen märker något förutom Google och andra sökmotorer som vill att
    man talar om var sajten lever. Detta syns i tex sitemap.xml filen.

--- src/navigation.js
+++ src/navigation.js
@@ -48,7 +48,7 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/blaudden/mtbo-sajten' },
   ],
-  footNote: `© <a class="text-primary hover:underline dark:text-gray-200" href="https://mountainbikeorientering.se/">mountainbikeorientering.se</a> 2023`
+  footNote: `© <a class="text-primary hover:underline dark:text-gray-200" href="https://www.mountainbikeorientering.se/">mountainbikeorientering.se</a> 2023`
 
 
 };
