7e6c5c933c7ae1abadfe778eb7c282f85ebc12eb
Magnus Blåudd <blaudden@users.noreply.github.com>
2023-11-24 20:58:04 +0100

    Lang och locale från url

---
+++ src/utils/i18n.ts
@@ -0,0 +1,30 @@
+export const languages = {
+  en: 'English',
+  sv: 'Svenska',
+};
+
+export const defaultLang = 'sv';
+
+export const ui = {
+  sv: {
+    'nav.home': 'Hem',
+    'nav.about': 'Om',
+    'nav.twitter': 'Twitter',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+} as const;
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
+}
