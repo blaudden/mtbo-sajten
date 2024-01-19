9c32b7c1e9d61593bfd3b90387137bd3b9115e15
Magnus Blåudd <blaudden@users.noreply.github.com>
2024-01-19 21:52:30 +0100

    Fixa länk till redirect

---
+++ resize-images.mjs
@@ -0,0 +1,15 @@
+import { functionOptimizeImages } from 'images-folder-optimizer';
+
+functionOptimizeImages({
+  stringOriginFolder: 'src/assets/images/',
+  stringDestinationFolder: 'src/assets/images',
+  arrayOriginFormats: ['jpg'],
+  arrayDestinationFormats: ['jpg'],
+  objectResizeOptions: {
+    width: 1024,
+    fit: 'contain',
+    withoutEnlargement: true
+  }
+}).then((results) => {
+  console.table(results);
+});
