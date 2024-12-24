4f2c9958ab4c70f4567b70d0719301890311934f
Magnus Blåudd <blaudden@users.noreply.github.com>
2024-12-25 00:12:48 +0100

    Eslint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -53,6 +53,6 @@ export default [
     },
   },
   {
-    ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts'],
+    ignores: ['dist', 'node_modules', '.github', 'types.generated.d.ts', '.astro'],
   },
 ];
